846760862dc13fc1c7dd312ef8a6eb60ea71da9a
DevSergei <39258174+DevSergei@users.noreply.github.com>
2018-05-28 09:56:47 +0300

    override getMessage
    Btw, I use an interface iMaxSizeContaining to avoid magic numbers  (iMaxSizeContaining.MAX_SIZE;)

--- ExceptionLesson/src/MyArraySizeException.java
+++ ExceptionLesson/src/MyArraySizeException.java
@@ -1,6 +1,13 @@
 public class MyArraySizeException extends Exception {
     public MyArraySizeException(){
         super();
-
+    }   
+    //We can do smth like this:
+    @Override
+    public String getMessage() {
+        if (idx != -1)
+            return "array[" + idx + "].length exceeds the MAX_SIZE of " + iMaxSizeContaining.MAX_SIZE; //super.getMessage();
+        else
+            return msg;
     }
 }
